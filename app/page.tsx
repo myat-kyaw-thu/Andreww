d11f9254d1811f9ee0fca08ecc4d616b3b50793a
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-03-06 22:20:16 +0630

    adjust page for pageloader

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
-import AboutSection from "./components/About";
-import { BlurFade } from "./components/BlurFade";
-import ContactSection from "./components/ContactSeciton";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import ProjectsSection from "./components/projects";
-import SkillsSection from "./components/Skills";
+"use client"
+
+import AboutSection from "./components/About"
+import { BlurFade } from "./components/BlurFade"
+import ContactSection from "./components/ContactSeciton"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import { PageLoader } from "./components/PageLoader"
+import ProjectsSection from "./components/projects"
+import SkillsSection from "./components/Skills"
 
 export default function Home() {
   return (
-    <div id="top">
-    <main className="min-h-screen bg-background font-sans max-w-2xl mx-auto py-12 sm:py-24 px-6 gap-y-9 pb-24">
-      <BlurFade delay={0.25} inView direction="up">
-        <Hero />
-      </BlurFade>
+    <PageLoader minDuration={4000}>
+      <div id="top">
+        <main className="min-h-screen bg-background font-sans max-w-2xl mx-auto py-12 sm:py-24 px-6 gap-y-9 pb-24">
+          <BlurFade delay={0.25} inView direction="up">
+            <Hero />
+          </BlurFade>
 
-      <BlurFade delay={0.45} inView direction="up">
-        <AboutSection />
-      </BlurFade>
-      <BlurFade delay={0.65} inView direction="up">
-        <ProjectsSection />
-      </BlurFade>
-        <SkillsSection />
-        <ContactSection />
-        <Footer/>
-    </main>
-  <Navbar />
-</div>
-  );
+          <BlurFade delay={0.45} inView direction="up">
+            <AboutSection />
+          </BlurFade>
+          <BlurFade delay={0.65} inView direction="up">
+            <ProjectsSection />
+          </BlurFade>
+          <SkillsSection />
+          <ContactSection />
+          <Footer />
+        </main>
+        <Navbar />
+      </div>
+    </PageLoader>
+  )
 }
+
